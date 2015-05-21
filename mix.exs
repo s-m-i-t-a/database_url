c0b1ad635fa97940fd52baaac6cc729867279fe3
Jindrich K. Smitka <jsmitka@smita.info>
2015-05-21 09:25:11 +0200

    Added excoveralls for covarege report

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule DatabaseUrl.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     test_coverage: [tool: ExCoveralls]
+    ]
   end
 
   # Configuration for the OTP application
@@ -27,6 +29,6 @@ defmodule DatabaseUrl.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:excoveralls, "~> 0.3", only: [:dev, :test]}]
   end
 end
