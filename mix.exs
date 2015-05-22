1a3947ddf8b3e19f2a70dbbfcb9ac4d6afcfebb8
Jindrich K. Smitka <jsmitka@smita.info>
2015-05-22 14:32:12 +0200

    Added settings to publish on hex.pm

--- mix.exs
+++ mix.exs
@@ -3,12 +3,14 @@ defmodule DatabaseUrl.Mixfile do
 
   def project do
     [app: :database_url,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
-     test_coverage: [tool: ExCoveralls]
+     test_coverage: [tool: ExCoveralls],
+     description: "Parse database URL and renturn keyword list for use with Ecto.",
+     package: package,
     ]
   end
 
@@ -16,7 +18,7 @@ defmodule DatabaseUrl.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
   # Dependencies can be Hex packages:
@@ -30,5 +32,16 @@ defmodule DatabaseUrl.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:excoveralls, "~> 0.3", only: [:dev, :test]}]
+  end
+
+  defp package do
+    [
+      files:        ["lib", "mix.exs", "README*", "LICENSE*"],
+      contributors: ["Jindrich K. Smitka <smitka.j@gmail.com>"],
+      licenses:     ["BSD"],
+      links:        %{
+        "GitHub" => "https://github.com/s-m-i-t-a/database_url",
+      }
+    ]
   end
 end
